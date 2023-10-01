7915b034cb7168067883b10aa5d25185be8d581b
Ali Amrahli <ali.emrahli@gmail.com>
2023-10-01 14:58:40 +0400

    Each time the user inputs into the client application, one of the randomly chosen server applications prints it.

--- client.py
+++ client.py
@@ -3,21 +3,16 @@ import random
 
 
 def client_program():
-    ports=[5000,5001,5002] # three different servers on these ports
+    ports = [5000, 5001, 5002]  # three different servers on these ports
 
-
-    message = ''
-    while message.strip() != 'stop': #program will continue untill user types stop
-        port = random.choice(ports)
-        host = socket.gethostname() # randomly chosing one of the servers
+    while True:  # program will continue until user types stop
+        port = random.choice(ports)  # randomly choosing one of the servers
         # creating client socket
         client_socket = socket.socket()
         host = socket.gethostname()
         client_socket.connect((host, port))  # send connection request to server
         message = input(" -> ")  # take input from user
-        client_socket.send(message.encode()) # send the input to server
-
-
+        client_socket.send(message.encode())  # send the input to server
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,26 +1,24 @@
 import socket
 import sys
 
+
 def server_program():
+    while True:
+        # creating server socket and binding the host and port number
+        host = socket.gethostname()
+        server_socket = socket.socket()
+        server_socket.bind((host, port))
 
-       # creating server socket and binding the host and port number
-       host = socket.gethostname()
-       server_socket = socket.socket()
-       server_socket.bind((host, port))
+        # server is listening to one client simultaneously
+        server_socket.listen(1)
 
-       #server is listening to one client simultaneously
-       server_socket.listen(1)
+        conn, address = server_socket.accept()  # accepting the connection request from client
+        print("Successfully connected to: " + str(address))
 
-       conn, address = server_socket.accept() #accepting the connection request from client
-       print("Successfully connected to: " + str(address))
+        data = conn.recv(1024).decode()  # getting data sent by client
+        print("Message from client: " + str(data))
 
-       while True:
-              data = conn.recv(1024).decode() #getting data sent by client
-              if not data:
-                     break #if no data received exit the loop and close connection
-              print("Message from client: " + str(data))
-       conn.close()
 
 if __name__ == '__main__':
-    port = int(sys.argv[1]) # take port number of server as an argument
+    port = int(sys.argv[1])  # take port number of server as an argument
     server_program()
